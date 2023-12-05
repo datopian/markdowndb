7e42ae252038813657974d73f6584072bddb0cde
mohamed yahia <mohamedsalem401@yahoo.com>
2023-12-05 03:14:26 +0200

    add npx markdowndb init CLI script that will bootstrap the config file

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import * as path from 'path';
+import { promises as fs } from 'fs';
 import { MarkdownDB } from "../lib/markdowndb.js";
 
 async function loadConfig(configFilePath) {
@@ -15,10 +16,27 @@ async function loadConfig(configFilePath) {
   }
 }
 
+async function initConfigFile(configFilePath = "mddb.config.js") {
+  const normalizedPath = path.resolve(configFilePath);
+  const configFileContent = `export default {\n  customFields: []\n};\n`;
+
+  try {
+    await fs.writeFile(normalizedPath, configFileContent);
+    console.log(`Configuration file created at ${normalizedPath}`);
+  } catch (error) {
+    throw new Error(`Error creating configuration file at ${normalizedPath}: ${error.message}`);
+  }
+}
+
 // TODO get these from markdowndb.config.js or something
 const dbPath = "markdown.db";
 const ignorePatterns = [/Excalidraw/, /\.obsidian/, /DS_Store/];
-const [contentPath, configFilePath] = process.argv.slice(2);
+const [command, contentPath, configFilePath] = process.argv.slice(2);
+
+if (command === 'init') {
+  await initConfigFile(configFilePath);
+  process.exit();
+}
 
 if (!contentPath) {
   throw new Error("Invalid/Missing path to markdown content folder");
