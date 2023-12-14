acb5183e04ce92401e10872e811a5a580a60f479
mohamed yahia <salemmohamed780@gmail.com>
2023-12-15 01:01:12 +0200

    [#55][s]: Implement basic approach to document types.

--- src/lib/CustomConfig.ts
+++ src/lib/CustomConfig.ts
@@ -1,6 +1,11 @@
 import { FileInfo } from "./process.js";
 import { Root } from "remark-parse/lib/index.js";
+import { ZodObject } from "zod";
+
+type ComputedFields = ((fileInfo: FileInfo, ast: Root) => any)[];
+type Schemas = { [index: string]: ZodObject<any> };
 
 export interface CustomConfig {
-  computedFields?: ((fileInfo: FileInfo, ast: Root) => any)[];
+  computedFields?: ComputedFields;
+  schemas?: Schemas;
 }
